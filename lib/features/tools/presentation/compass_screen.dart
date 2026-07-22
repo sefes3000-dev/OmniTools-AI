24f9b5d6b9630746a186df8625accf2aed18a5de
sefes3000-dev <sefes3000@gmail.com>
2026-07-22 16:10:31 +0100

    Create compass_screen.dart

---
+++ lib/features/tools/presentation/compass_screen.dart
@@ -0,0 +1,51 @@
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+
+class CompassScreen extends StatefulWidget {
+  const CompassScreen({super.key});
+
+  @override
+  State<CompassScreen> createState() => _CompassScreenState();
+}
+
+class _CompassScreenState extends State<CompassScreen> {
+  double? _heading = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterCompass.headingStream?.listen((event) {
+      if (mounted) {
+        setState(() => _heading = event.heading);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Compass')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '${_heading?.toStringAsFixed(0) ?? 0}°',
+              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            Transform.rotate(
+              angle: ((_heading ?? 0) * (math.pi / 180) * -1),
+              child: const Icon(
+                Icons.navigation,
+                size: 200,
+                color: Colors.redAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
