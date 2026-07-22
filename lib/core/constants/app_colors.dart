b5d95ca210702e9eb4a06e992893c4c976b9b454
sefes3000-dev <sefes3000@gmail.com>
2026-07-22 13:42:24 +0100

    Create app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6366F1);
+  static const Color secondary = Color(0xFF0EA5E9);
+  static const Color accent = Color(0xFFF59E0B);
+
+  static const Color lightBackground = Color(0xFFF8FAFC);
+  static const Color lightCard = Color(0xFFFFFFFF);
+
+  static const Color darkBackground = Color(0xFF0F172A);
+  static const Color darkCard = Color(0xFF1E293B);
+}
